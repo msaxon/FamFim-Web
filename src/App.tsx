63a65d9fbbd79aadbaeb5419cbe9dfbfddb24b84
Matthew Saxon <maestromattaeus@gmail.com>
2026-02-21 23:53:29 -0800

    updating to use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { LoginScreen } from './screens/LoginScreen';
 import { BudgetsScreen } from './screens/BudgetsScreen';
